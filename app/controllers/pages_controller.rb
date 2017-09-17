f05ee6229e59e9b327a73b036f47ca8120540ea4
Dolph Rails School <dolphik@gmail.com>
2017-09-17 11:32:21 +0000

    Pierwsza wersja po dodaniu Home i About

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+   
+   #definicja dla strony home
+   def home
+       
+   end
+   
+   #definicja dla strony about
+   def about
+       
+   end
+end
